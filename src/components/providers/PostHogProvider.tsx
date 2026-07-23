f09b54d2f290723d35e60d9fad0b00308cf542b4
harshwardhan26 <harshwardhan.17891@sakec.ac.in>
2026-07-24 02:25:35 +0530

    Fix PostHog initialization error when key is missing

--- src/components/providers/PostHogProvider.tsx
+++ src/components/providers/PostHogProvider.tsx
@@ -5,9 +5,9 @@ import { PostHogProvider as PHProvider } from 'posthog-js/react';
 import { usePathname, useSearchParams } from 'next/navigation';
 import { useEffect, Suspense } from 'react';
 
-if (typeof window !== 'undefined') {
-  posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY as string, {
-    api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST,
+if (typeof window !== 'undefined' && process.env.NEXT_PUBLIC_POSTHOG_KEY) {
+  posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY, {
+    api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST || 'https://us.i.posthog.com',
     person_profiles: 'identified_only',
     capture_pageview: false, // Disable automatic pageview capture, as we capture manually
   });
